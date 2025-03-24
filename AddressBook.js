aeb81be1142027e3217e272e4e62df2d0a8afdc2
kgupta01 <khushinu21@gmail.com>
2025-03-24 11:58:31 +0530

    Delete a contact

--- AddressBook.js
+++ AddressBook.js
@@ -111,6 +111,22 @@ function editContact(findName,editedVariable,variableNewValue){
                         break;      
                 }
             }
-        })
+
+        })       
+    }
+}
+
+//UC5
+//method to delete
+function deleteContact(first_Name){
+    if(addressBook.length == null){
+        console.log("Add Contact In Address Book");
+    }else{
+        for(let i = 0; i <addressBook.length ; i++){
+            if(addressBook[i].firstName == first_Name){
+                addressBook.splice(i,1);
+                console.log("Contact Deleted Successfully");
+            }
+        }
     }
 }
