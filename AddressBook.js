beb4fa23cfa4bd5a34fcabc880f2efa4580f4347
kgupta01 <khushinu21@gmail.com>
2025-03-24 11:53:20 +0530

    Find existing contact and update it

--- AddressBook.js
+++ AddressBook.js
@@ -74,3 +74,43 @@ function contactDetails(firstName,lastName,address,state,city,zipCode,email,phon
 //UC3 
 contactDetails('Khushi', 'Gupta', 'Mohammad Mah', 'Uttar Pradesh', 'Shikohabad', "283135", 'khushinu21@gmail.com', "9634921530");
 console.log(addressBook);
+
+//UC4
+
+//method to find and editContacts
+function editContact(findName,editedVariable,variableNewValue){
+    if(addressBook.length == null){
+        console.log("Add Contact In Address Book");
+    }else{
+        addressBook.forEach(newContact => {
+            if(newContact.firstName == findName){
+                switch(editedVariable){
+                    case "firstName":
+                        newContact.firstName = variableNewValue;
+                        break;
+                    case "lastName":
+                        newContact.lastName = variableNewValue;
+                        break;
+                    case "address":
+                        newContact.address = variableNewValue;
+                        break;
+                    case "state":
+                        newContact.state = variableNewValue;
+                        break;
+                    case "city":
+                        newContact.city = variableNewValue;
+                        break;
+                    case "zipCode":
+                        newContact.zipCode = variableNewValue;
+                        break;  
+                    case "firstName":
+                        newContact.firstName = variableNewValue;
+                        break;
+                    case "lastName":
+                        newContact.lastName = variableNewValue;
+                        break;      
+                }
+            }
+        })
+    }
+}
