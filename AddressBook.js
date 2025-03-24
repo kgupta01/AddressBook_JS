3bbed3ed8eafb2abef39b4f66e04f6614b241a24
kgupta01 <khushinu21@gmail.com>
2025-03-24 11:47:30 +0530

    check valid contacts are added

--- AddressBook.js
+++ AddressBook.js
@@ -1,21 +1,73 @@
-class Contact {
-    constructor(firstName, lastName, address, city, state, zip, phone, email) {
-      if (!/^[A-Z][a-zA-Z]{2,}$/.test(firstName)) throw new Error("Invalid First Name");
-      if (!/^[A-Z][a-zA-Z]{2,}$/.test(lastName)) throw new Error("Invalid Last Name");
-      if (!/^[A-Za-z0-9 ]{4,}$/.test(address)) throw new Error("Invalid Address");
-      if (!/^[A-Za-z]{4,}$/.test(city)) throw new Error("Invalid City");
-      if (!/^[A-Za-z]{4,}$/.test(state)) throw new Error("Invalid State");
-      if (!/^\d{5}$/.test(zip)) throw new Error("Invalid Zip Code");
-      if (!/^\d{10}$/.test(phone)) throw new Error("Invalid Phone Number");
-      if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) throw new Error("Invalid Email");
-  
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.address = address;
-      this.city = city;
-      this.state = state;
-      this.zip = zip;
-      this.phone = phone;
-      this.email = email;
+// UC-1
+class Contact{
+    firstName;
+    lastName;
+    address;
+    state;
+    city;
+    zipCode;
+    email;
+    
+    phoneNumber;
+
+   //created constructor
+    constructor(firstName,lastName,address,state,city,zipCode,email,phoneNumber){
+        this.firstName = firstName
+        this.lastName = lastName
+        this.address = address
+        this.state = state
+        this.city = city
+        this.zipCode = zipCode
+        this.email = email
+        this.phoneNumber = phoneNumber
     }
-  }
+}
+
+//UC2
+var addressBook = new Array();
+
+function contactDetails(firstName,lastName,address,state,city,zipCode,email,phoneNumber){
+    
+    //checking duplicate
+    addressBook.filter(contact => contact.firstName == firstName)
+    .reduce(() => count++ , count = 0);
+    if(count > 0){
+        console.log("Contact With Name " + firstName + " Already Present")
+    }else{
+        const firstNamePattern = /^[A-Z][a-zA-Z]{3,}/;
+    let firstName_Check = firstNamePattern.test(firstName);
+
+    const lastNamePattern = /^[A-Z][a-zA-Z]{3,}/;
+    let lastName_Check = lastNamePattern.test(lastName);
+
+    const addressPattern = /^[A-Za-zA-Z0-9]{3,}/;
+    let address_Check = addressPattern.test(address);
+
+    const statePattern = /^[A-Za-zA-Z]{3,}/;
+    let state_Check = statePattern.test(state);
+
+    const cityPattern = /^[A-Za-zA-Z]{3,}/;
+    let city_Check = cityPattern.test(city);
+
+    const zipCodePattern = /^[0-9]{6}/;
+    let zipCode_Check = zipCodePattern.test(zipCode) ;
+
+    const emailPattern = /^[A-Za-z0-9]+(.[A-Za-z0-9]+)@[^\\W]+(.[^\\W]+)?(?=(.[^_\\W]{3,}$|.[a-zA-Z]{2}$)).$/;
+    let email_Check = emailPattern.test(email);
+
+    const phoneNumberPattern = /^[0-9]{10}/;
+    let phoneNumber_Check = phoneNumberPattern.test(phoneNumber);
+
+        if(firstName_Check == true && lastName_Check == true && address_Check == true && state_Check == true && city_Check == true
+                        && zipCode_Check == true && email_Check == true && phoneNumber_Check == true){
+           
+            let newContact = new Contact(firstName,lastName,address,state,city,zipCode,email,phoneNumber);
+            console.log("Contact Added Successfully");
+            
+            addressBook.push(newContact);
+
+        }else{
+            throw 'Contact Details Are Invalid';
+        }
+    }   
+}
